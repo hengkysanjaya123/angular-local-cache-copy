f00ee870d3020d26fea20d817b18e974b42a0007
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2020-10-16 20:06:34 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Post, PostService} from '../services/post.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-local-cache-copy';
+  posts: Post[];
+
+  constructor(private postService: PostService) {
+    this.postService.getPosts().subscribe(res => {
+      this.posts = res;
+    });
+  }
 }
---
+++ src/services/post.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {delay, startWith} from 'rxjs/operators';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+
+  url = 'https://jsonplaceholder.typicode.com/posts';
+  initialPosts: Post[] = [{
+    id: 0,
+    title: 'test 1',
+    userId: 1,
+    body: 'body 1'
+  }];
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.url).pipe(delay(3000), startWith(this.initialPosts));
+  }
+}
